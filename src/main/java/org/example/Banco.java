8a59e2159d7a0061aa29468720a7862d6dd46384
Giovandro Santos <vsm.giovandro@gmail.com>
2024-09-04 08:19:31 -0300

    first commit

---
+++ src/main/java/org/example/Banco.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Banco {
+    private String nome;
+    private List<Conta> contas;
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        Cliente giovandro = new Cliente();
+        giovandro.setNome("Giovandro");
+
+        Conta cc = new ContaCorrente(giovandro);
+        Conta poupanca = new ContaPoupanca(giovandro);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+}
